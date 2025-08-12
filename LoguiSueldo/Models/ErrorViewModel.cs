ed63a83c962663a6eb9cb01727792f5e73c82858
Federico-Audizio <federicoaudizio08@gmail.com>
2025-08-12 11:32:14 -0300

    HolaMundo

---
+++ LoguiSueldo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoguiSueldo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
