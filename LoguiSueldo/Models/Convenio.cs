b2792b363bbbee84ce48c8e651aa3edd361e779b
CristianN-22 <cris2210.jornada@outlook.com>
2025-08-13 09:40:36 -0300

    prueba git

---
+++ LoguiSueldo/Models/Convenio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System;
+
+namespace LoguiSueldo.Models
+{
+	public class Convenio
+	{
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int IdConvenio { get; set; }
+
+
+    }
+}
