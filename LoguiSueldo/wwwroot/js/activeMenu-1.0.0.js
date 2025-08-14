397a03fc9f0ac2b8b0803bb44e73363f343f9ee9
Federico-Audizio <federicoaudizio08@gmail.com>
2025-08-14 08:34:54 -0300

    PrimerasVistas 0.1

---
+++ LoguiSueldo/wwwroot/js/activeMenu-1.0.0.js
@@ -0,0 +1,66 @@
+﻿///** add active class and stay opened when selected */
+//var url = window.location;
+
+//// for sidebar menu entirely but not cover treeview
+//$('ul.sidebar-menu a').filter(function () {
+//    return this.href == url;
+//}).parent().addClass('active');
+
+//// for treeview
+//$('ul.treeview-menu a').filter(function () {
+//    return this.href == url;
+//}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
+
+
+//// for sidebar menu entirely but not cover treeview
+//$('li.sidebar-menu a').filter(function () {
+//    return this.href == url;
+//}).parent().addClass('active');
+
+//// for treeview
+//$('li.treeview-menu a').filter(function () {
+//    return this.href == url;
+//}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
+
+
+
+
+/** add active class and stay opened when selected */
+//var url = window.location;
+var res = encodeURI(window.location);
+var vectorUrl = res.split("/", 5);
+
+if (vectorUrl[4] == "Create" || vectorUrl[4] == "Edit" || vectorUrl[4] == "ImprimirCobro") {
+    vectorUrl[4] = "Index";
+}
+else if (vectorUrl[4] == "" || vectorUrl[4] == null) {
+    vectorUrl[4] = "Index";
+}
+//if (vectorUrl[4] == "Create")
+//{
+//    vectorUrl[4] = "Create";
+//}
+//else if (vectorUrl[4] == "Edit")
+//{
+//    vectorUrl[4] = "Edit";
+//}
+//else {
+
+//}
+//var url = "http://" + vectorUrl[2] + "/" + vectorUrl[3] + "/Index";
+var url = "http://" + vectorUrl[2] + "/" + vectorUrl[3] + "/" + vectorUrl[4];
+
+// for sidebar menu entirely but not cover treeview
+$('ul.sidebar-menu a').filter(function () {
+    return this.href == url;
+}).parent().addClass('active');
+
+//Top bar
+$('ul.navbar-nav a').filter(function () {
+    return this.href == url;
+}).parent().addClass('active');
+
+// for treeview
+$('ul.treeview-menu a').filter(function () {
+    return this.href == url;
+}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
---
+++ LoguiSueldo/wwwroot/js/tamanioFactura.js
@@ -0,0 +1,5 @@
+﻿if (max-width == 1300px){
+
+}
+var elemento = document.getElementById("images");
+elemento.className += " col-md-6";
